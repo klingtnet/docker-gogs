07c74c61f705a4b14fbbcb01e77df5de269cc3c6
Andreas Linz <klingt.net@gmail.com>
2015-04-08 14:39:36 +0200

    changed the name of the docker container to gogs

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ GOGS_SHARE_PATH=${GOGS_SHARE_PATH:-${SCRIPT_ROOT}/share}
 GOGS_SSH_PORT=${GOGS_SSH_PORT:-18022}
 GOGS_HTTP_PORT=${GOGS_HTTP_PORT:-18000}
 
-docker run --name docker-gogs --rm --volume ${GOGS_SHARE_PATH}:/home/gogs/share --publish ${GOGS_SSH_PORT}:22 --publish ${GOGS_HTTP_PORT}:8080 gogs /home/gogs/start.sh
+docker run --name gogs --rm --volume ${GOGS_SHARE_PATH}:/home/gogs/share --publish ${GOGS_SSH_PORT}:22 --publish ${GOGS_HTTP_PORT}:3000 gogs
 
